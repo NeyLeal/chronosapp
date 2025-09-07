e56bbfdc0fd6f5332259a8744a837ddea7624603
Ney Leal <oliveira.ney7@gmail.com>
2025-09-07 19:52:43 -0300

    campo input e mais css, ainda sem as funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from './components/Container'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
+import { Input } from './components/Input'
 
 export function App() {
   return (
@@ -17,6 +18,31 @@ export function App() {
       </Container>
       <Container>
         <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action="">
+          <div className="formRow">
+            <Input 
+            id='input' 
+            type='text' 
+            labelText='Task' 
+            placeholder='Digite a tarefa'
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Texto abaixo</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos:</p>
+            <p>BOLIHAS</p>
+          </div>
+
+          <div className="formRow">
+           <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   )
